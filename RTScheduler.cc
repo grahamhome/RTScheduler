53d20665f3696cfd70a2cf9ab9084254801e1191
Graham Home <gmh5970@rit.edu>
2017-09-30 13:28:37 -0400

    Added task input functionality

--- RTScheduler.cc
+++ RTScheduler.cc
@@ -2,12 +2,30 @@
 #include <iostream>
 #include <Task.cc>
 #include <vector>
+#include <string>
 using namespace std;
 
 #define NUM_TASKS 10
 vector<Task*> tasks;
 
 Task* (*algorithm)();
+
+/* Method run by a task during execution */
+void* execute(void* task) {
+	return NULL;
+}
+
+/* Sets up a new thread */
+void createThread(pthread_t* thread, Task* task) {
+	pthread_attr_t attributes;
+	pthread_attr_init(&attributes);
+	int priority = 1; // Default starting priority for all tasks
+	struct sched_param param;
+	pthread_attr_getschedparam(&attributes, &param);
+	param.sched_priority = priority;
+	pthread_attr_setschedparam(&attributes, &param);
+	pthread_create(thread, &attributes, execute, (void*)task);
+}
 
 Task* EDF(){
 	Task* earliestTask = NULL;
@@ -56,7 +74,7 @@ int main(int argc, char *argv[]) {
 	printf("Scheduling algorithm choices:\n(0) Earliest Deadline First\n(1) Shortest Completion Time\n(2) Least Slack Time\n");
 	printf("Choose a scheduling algorithm (enter the number of your choice): ");
 	int selection = -1;
-	while (!(selection>=0 && selection <3)) {
+	while (!(selection>=0 && selection <=2)) {
 		cin >> selection;
 	}
 	printf("You selected ");
@@ -71,5 +89,54 @@ int main(int argc, char *argv[]) {
 			printf("Least Slack Time\n");
 			break;
 	}
+	printf("Enter tasks in the format \'<name> <execution time> <deadline> <period>\' and follow each with a newline.\nTo quit, enter \'start\' followed by a newline.\n");
+	string input;
+	getline(cin, input); // Ignore newline from previous println
+	bool done = false;
+	while (!done) {
+		getline(cin, input);
+		if (input == "start") {
+			done = true;
+			break;
+		}
+		string name;
+		int c;
+		int p;
+		int d;
+		int tokenCount = 0;
+		size_t pos = 0;
+		while (((pos = input.find(" ")) != string::npos) && tokenCount < 4) {
+			switch(tokenCount) {
+			case 0:
+				name = input.substr(0, pos);
+				tokenCount++;
+				break;
+			case 1:
+				c = atoi(input.substr(0, pos).c_str());
+				tokenCount++;
+				break;
+			case 2:
+				p = atoi(input.substr(0, pos).c_str());
+				tokenCount++;
+				break;
+			case 3:
+				d = atoi(input.substr(0, pos).c_str());
+				tokenCount++;
+				break;
+			default:
+				tokenCount++;
+				break;
+			}
+		}
+		if (tokenCount != 4) {
+			printf("Invalid task format.\n");
+		} else {
+			pthread_t* taskThread = NULL;
+			Task* task = new Task(name, c, p, d, taskThread);
+			createThread(taskThread, task);
+			tasks.push_back(task);
+		}
+	}
+	printf("%d", tasks.size());
 	return EXIT_SUCCESS;
 }
